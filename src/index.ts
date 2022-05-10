3855611e9d1ad408ecf54e9bcef9c059febe6943
Fernanda Costa <fernandacosta@alu.ufc.br>
2022-05-09 22:16:46 -0300

    feat: post, put and delete methods

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import usersRoute from './routes/users.route';
 
 const app = express();
 
+// Aplication Settings
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Route Settings
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ foo: 'bar' });
+  res.status(200).send({ foo: 'Sucess!' });
 });
 
+// Server Startup
 app.listen(3000, () => {
   console.log('Running aplication on port 3000!!!');
 });
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,15 +1,33 @@
 import { Router, Request, Response, NextFunction, response } from "express";
+import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
   const users = [{ userName: 'Fer' }];
-  res.status(200).send({users});
+  res.status(StatusCodes.OK).send(users);
 });
 
 usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
   const uuid = req.params.uuid;
-  res.status(200).send({ uuid });
+  res.status(StatusCodes.OK).send({ uuid });
+});
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body;
+  res.status(StatusCodes.CREATED).send(newUser);
+});
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const modifiedUser = req.body;
+  const uuid = req.params.uuid;
+
+  modifiedUser.uuid = uuid;
+  res.status(StatusCodes.OK).send({ uuid });
+});
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  res.sendStatus(StatusCodes.OK);
 });
 
 export default usersRoute;
